15747babc603cc7e889794d0130d0ce0c1709d92
Nolwenn <sachet.nolwenn@orange.fr>
2019-11-25 10:19:52 +0100

    findAll

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -2,6 +2,7 @@
 // src/Controller/WildController.php
 namespace App\Controller;
 
+use App\Entity\Program;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,12 +10,26 @@ use Symfony\Component\Routing\Annotation\Route;
 Class WildController extends AbstractController
 {
     /**
+     * Show all rows from Program's entity
+     *
      * @Route("/wild", name="wild_index")
+     * @return Response A response instance
      */
     public function index(): Response
     {
+        $programs = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findAll();
+
+        if (!$programs) {
+            throw $this->createNotFoundException(
+                'No program found in program\'s table.'
+            );
+        }
+
         return $this->render('wild/index.html.twig', [
             'website' => 'Wild Séries',
+            'programs' => $programs
         ]);
     }
 
