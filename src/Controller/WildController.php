be541453c97584b22c926949a49df2b9fc109d6d
Nolwenn <sachet.nolwenn@orange.fr>
2019-11-25 10:33:39 +0100

    findByOne

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -34,13 +34,33 @@ Class WildController extends AbstractController
     }
 
     /**
-     * @Route("/wild/show/{slug}", requirements={"slug":"[a-z0-9\-]+"}, defaults={"slug" = ""},  name="wild_show")
+     * Getting a program with a formatted slug for title
+     *
+     * @param string $slug The slugger
+     * @Route("/wild/show/{slug<^[a-z0-9-]+$}", defaults={"slug" = null},  name="wild_show")
      */
 
     public function show(string $slug): Response
     {
-        $slug = str_replace("-", " ", $slug);
-        $slug = ucwords($slug);
+        if(!$slug) {
+            throw $this->createNotFoundException(
+                'No slug has been sent to find a program in program\'s table .'
+            );
+        }
+
+        $slug = preg_replace(
+            '/-/',
+            ' ', ucwords(trim(strip_tags($slug)))
+        );
+
+        $program = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findOneBy(['title' => mb_strtolower($slug)]);
+        if (!$program) {
+            throw $this->createNotFoundException(
+                'No program with ' . $slug . ' title, found in program\'s table.'
+            );
+        }
 
         return $this->render('wild/show.html.twig', [
             'website' => 'Wild Séries',
