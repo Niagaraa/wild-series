cc844fb465caa4e06f2e5aa83fced0e7de30196b
Nolwenn <sachet.nolwenn@orange.fr>
2019-11-25 09:58:14 +0100

    Relation Many-to-One

--- src/Migrations/Version20191125085636.php
+++ src/Migrations/Version20191125085636.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20191124182544 extends AbstractMigration
+final class Version20191125085636 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,7 +22,10 @@ final class Version20191124182544 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(100) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE category CHANGE name name VARCHAR(100) NOT NULL');
+        $this->addSql('ALTER TABLE program ADD category_id INT NOT NULL');
+        $this->addSql('ALTER TABLE program ADD CONSTRAINT FK_92ED778412469DE2 FOREIGN KEY (category_id) REFERENCES category (id)');
+        $this->addSql('CREATE INDEX IDX_92ED778412469DE2 ON program (category_id)');
     }
 
     public function down(Schema $schema) : void
@@ -30,6 +33,9 @@ final class Version20191124182544 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP TABLE category');
+        $this->addSql('ALTER TABLE category CHANGE name name VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE program DROP FOREIGN KEY FK_92ED778412469DE2');
+        $this->addSql('DROP INDEX IDX_92ED778412469DE2 ON program');
+        $this->addSql('ALTER TABLE program DROP category_id');
     }
 }
