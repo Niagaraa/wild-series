3df6c2892c48bbefce22f68b52240ef27aa80f85
Nolwenn <sachet.nolwenn@orange.fr>
2019-12-08 23:07:51 +0100

    mise en page

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,7 +5,7 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-// any CSS you require will output into a single css file (app.scss in this case)
+// any SCSS you require will output into a single css file (app.scss in this case)
 require('../css/app.scss');
 
 const $ = require('jquery');
