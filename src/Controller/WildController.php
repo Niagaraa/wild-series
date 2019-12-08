06f6a80195fe66aa043225e1a85df7031742407d
Nolwenn <sachet.nolwenn@orange.fr>
2019-12-08 22:02:11 +0100

    param converter

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -9,6 +9,7 @@ use App\Entity\Season;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 Class WildController extends AbstractController
 {
