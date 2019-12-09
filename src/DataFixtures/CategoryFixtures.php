c265caf5446dc309719fa4257c91e8ee8c2ffb9a
Nolwenn <sachet.nolwenn@orange.fr>
2019-12-09 16:48:19 +0100

    first fixture

---
+++ src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class CategoryFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $category = new Category();
+        $category->setName('Nom de catégorie');
+        $manager->persist($category);
+        $manager->flush();
+    }
+}
