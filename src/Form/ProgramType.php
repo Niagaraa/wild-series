4b02e21437dc449ee381a15e3fc1e1db000505b0
Nolwenn <sachet.nolwenn@orange.fr>
2019-12-09 15:52:11 +0100

    les acteurs apparaissent dans un select du formulaire

--- src/Form/ProgramType.php
+++ src/Form/ProgramType.php
@@ -6,6 +6,8 @@ use App\Entity\Program;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use App\Entity\Actor;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class ProgramType extends AbstractType
 {
@@ -16,6 +18,10 @@ class ProgramType extends AbstractType
             ->add('summary')
             ->add('poster')
             ->add('category', null, ['choice_label' => 'name'])
+            ->add('actors', EntityType::class, [
+                'class' => Actor::class,
+                'choice_label' => 'name',
+            ])
         ;
     }
 
