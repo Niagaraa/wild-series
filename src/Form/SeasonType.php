b1ce9c52eef2d65eac4de889c1250c98108ce16c
Nolwenn <sachet.nolwenn@orange.fr>
2019-12-09 09:29:52 +0100

    add Season

--- src/Form/SeasonType.php
+++ src/Form/SeasonType.php
@@ -9,15 +9,13 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class SeasonType extends AbstractType
 {
-    protected $program;
-
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('year')
             ->add('description')
             ->add('number')
-            ->add('program', null, ['choice_label' => 'program']);
+            ->add('program', null, ['choice_label' => 'title']);
     }
 
     public function configureOptions(OptionsResolver $resolver)
