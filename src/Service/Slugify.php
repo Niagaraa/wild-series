7d2cfbc635fa506fd01b07ef7b4d8a310aaa687e
Nolwenn <sachet.nolwenn@orange.fr>
2019-12-12 15:08:04 +0100

    pas de ponctuation

--- src/Service/Slugify.php
+++ src/Service/Slugify.php
@@ -11,6 +11,8 @@ class Slugify
         $input = strtolower(str_replace(' ', '-', $input));
         $input = str_replace('--', '-', $input);
         $input = iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE', $input);
+        $input = preg_replace('#[^\w]+#i', '-', $input);
+        $input = trim($input, '-');
 
         return $input;
     }
