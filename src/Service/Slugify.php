89fa92e51dd39f322f6f40a512b565110c651099
Nolwenn <sachet.nolwenn@orange.fr>
2019-12-12 15:07:19 +0100

    accent etc remplacés par lettres normales

--- src/Service/Slugify.php
+++ src/Service/Slugify.php
@@ -10,8 +10,8 @@ class Slugify
     {
         $input = strtolower(str_replace(' ', '-', $input));
         $input = str_replace('--', '-', $input);
+        $input = iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE', $input);
 
         return $input;
     }
-
 }
