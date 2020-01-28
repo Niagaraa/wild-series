93d9fdf96d1cd9697683323bf7025fa201dff3e3
Nolwenn <sachet.nolwenn@orange.fr>
2020-01-28 01:10:08 +0100

    profil membre

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -4,19 +4,23 @@
 namespace App\Controller;
 
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class Profile extends AbstractController
+class ProfileController extends AbstractController
 {
     /**
      * @Route("/my-profile", name="user_profile")
+     * @IsGranted("ROLE_SUBSCRIBER")
      */
 
     public function profile(): Response
     {
-        return $this->render('profile/profile.html.twig');
+        return $this->render('profile/profile.html.twig', [
+            'user' => $this->getUser()
+        ]);
     }
 
 }
